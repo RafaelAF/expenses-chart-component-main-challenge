623c6c9bf1dacd08422c2c9d05d0334b523bd268
Antonio Rafael <andrade.rafa71@gmail.com>
2022-06-01 21:44:49 -0300

    Project in 99%

--- script.js
+++ script.js
@@ -5,38 +5,37 @@ let maior = 0;
     let dias;
     let num;
 
-      fetch("./data.json")
-        .then(function (resp) {
-          return resp.json();
-        })
-        .then(function(data){
-          const dados = data;
-          dados.forEach((props, index, array) => {
-            const percent = props.amount;
-            const days = props.day;
-            if(percent>maior){
-              maior = percent;
-              indice = index;
-            }
-            document.querySelectorAll('.height')[index].style.height = (2 * percent)+'px';
-            document.querySelectorAll('.text-muted')[index].innerHTML = days;
-            document.querySelectorAll('.height')[index].setAttribute('title', percent);
+fetch("./data.json")
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function(data){
+    const dados = data;
+    dados.forEach((props, index) => {
+      const percent = props.amount;
+      const days = props.day;
+      if(percent>maior){
+        maior = percent;
+        indice = index;
+      }
+      document.querySelectorAll('.height')[index].style.height = (2 * percent)+'px';
+      document.querySelectorAll('.text-muted')[index].innerHTML = days;
+      document.querySelectorAll('.height')[index].setAttribute('title', percent);
+      
+      // fazer verificação do maior para fazer efeito hover
+      
 
-            // fazer verificação do maior para fazer efeito hover
-            
-
-            /*console.log(percent) // valores do json item a item
-            console.log(days)*/
-          });
-          document.querySelectorAll('.height')[indice].style.backgroundColor = 'hsl(186, 34%, 60%)';
-          document.querySelectorAll('.height')[indice].classList.add('maior');
-          
-        })
-        .catch((error)=>{
-          console.log('deu problema')
-          console.log(error)
-        })
-        /*$(function () {
-          $('[data-bs-toggle="tooltip"]').tooltip()
-        })*/
+      /*console.log(percent) // valores do json item a item
+      console.log(days)*/
+    });
+    document.querySelectorAll('.height')[indice].style.backgroundColor = 'hsl(186, 34%, 60%)';
+    document.querySelectorAll('.height')[indice].classList.add('maior');
     
+  })
+  .catch((error)=>{
+    console.log('deu problema')
+    console.log(error)
+  })
+  /*$(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip()
+  })*/
